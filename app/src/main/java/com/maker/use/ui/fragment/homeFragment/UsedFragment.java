9bbfe3e9fb31332d6b65e79323f3b9889922d743
XT <762348474@qq.com>
2016-10-22 09:34:27 +0800

    添加兴趣专题模块

--- app/src/main/java/com/maker/use/ui/fragment/homeFragment/UsedFragment.java
+++ app/src/main/java/com/maker/use/ui/fragment/homeFragment/UsedFragment.java
@@ -9,7 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.lzy.widget.HeaderScrollHelper;
@@ -36,19 +38,61 @@ import java.util.HashMap;
  * Created by XT on 2016/10/21.
  */
 
-public class UsedFragment extends BaseFragment implements HeaderScrollHelper.ScrollableContainer {
+public class UsedFragment extends BaseFragment implements HeaderScrollHelper.ScrollableContainer, View.OnClickListener {
 
     @ViewInject(R.id.cl_root)
     CoordinatorLayout cl_root;
+    //hobbyView
+    @ViewInject(R.id.ll_hobby_1)
+    LinearLayout ll_hobby_1;
+    @ViewInject(R.id.ll_hobby_2)
+    LinearLayout ll_hobby_2;
+    @ViewInject(R.id.ll_hobby_3)
+    LinearLayout ll_hobby_3;
+    @ViewInject(R.id.ll_hobby_4)
+    LinearLayout ll_hobby_4;
+    @ViewInject(R.id.ll_hobby_5)
+    LinearLayout ll_hobby_5;
+    @ViewInject(R.id.ll_hobby_6)
+    LinearLayout ll_hobby_6;
+    @ViewInject(R.id.iv_hobby_1)
+    ImageView iv_hobby_1;
+    @ViewInject(R.id.tv_hobby_1)
+    TextView tv_hobby_1;
+    @ViewInject(R.id.iv_hobby_2)
+    ImageView iv_hobby_2;
+    @ViewInject(R.id.tv_hobby_2)
+    TextView tv_hobby_2;
+    @ViewInject(R.id.iv_hobby_3)
+    ImageView iv_hobby_3;
+    @ViewInject(R.id.tv_hobby_3)
+    TextView tv_hobby_3;
+    @ViewInject(R.id.iv_hobby_4)
+    ImageView iv_hobby_4;
+    @ViewInject(R.id.tv_hobby_4)
+    TextView tv_hobby_4;
+    @ViewInject(R.id.iv_hobby_5)
+    ImageView iv_hobby_5;
+    @ViewInject(R.id.tv_hobby_5)
+    TextView tv_hobby_5;
+    @ViewInject(R.id.iv_hobby_6)
+    ImageView iv_hobby_6;
+    @ViewInject(R.id.tv_hobby_6)
+    TextView tv_hobby_6;
     @ViewInject(R.id.pagerHeader)
     private LoopViewPager pagerHeader;
     @ViewInject(R.id.ci)
     private CircleIndicator ci;
-
     private ArrayList<Top.img> mImgs;
     private MyXRecyclerView mMyXRecyclerView;
     private MainActivity mActivity;
     private View mMainView;
+    private String[] mTitleArray = {"#游戏专题","#考研必备","#音乐就是生命","#电子爱好者","#摄影艺术家","#嘻哈一族"};
+    private int[] mImgArray = {
+            R.mipmap.hobby_1, R.mipmap.hobby_2,
+            R.mipmap.hobby_3, R.mipmap.hobby_4,
+            R.mipmap.hobby_5, R.mipmap.hobby_6,
+    };
 
     @Nullable
     @Override
@@ -90,9 +134,10 @@ public class UsedFragment extends BaseFragment implements HeaderScrollHelper.Scr
         mMyXRecyclerView.addHeaderView(headerView);
 
         //添加头布局（店铺）
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.list_item_header_used_hobby, (ViewGroup) getActivity().findViewById(android.R.id.content), false);
-//        View view = View.inflate(UIUtils.getContext(), R.layout.list_item_header_used_hobby, null);
-        mMyXRecyclerView.addHeaderView(view);
+        View hobbyView = LayoutInflater.from(getActivity()).inflate(R.layout.list_item_header_used_hobby, (ViewGroup) getActivity().findViewById(android.R.id.content), false);
+        x.view().inject(this, hobbyView);
+        initHobbyView();
+        mMyXRecyclerView.addHeaderView(hobbyView);
         //添加监听，在用户滑动到下面时停止图片轮播，节省ui刷新
         mMyXRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -112,6 +157,28 @@ public class UsedFragment extends BaseFragment implements HeaderScrollHelper.Scr
                 }
             }
         });
+    }
+
+    private void initHobbyView() {
+        ll_hobby_1.setOnClickListener(this);
+        ll_hobby_2.setOnClickListener(this);
+        ll_hobby_3.setOnClickListener(this);
+        ll_hobby_4.setOnClickListener(this);
+        ll_hobby_5.setOnClickListener(this);
+        ll_hobby_6.setOnClickListener(this);
+
+        iv_hobby_1.setImageResource(mImgArray[0]);
+        iv_hobby_2.setImageResource(mImgArray[1]);
+        iv_hobby_3.setImageResource(mImgArray[2]);
+        iv_hobby_4.setImageResource(mImgArray[3]);
+        iv_hobby_5.setImageResource(mImgArray[4]);
+        iv_hobby_6.setImageResource(mImgArray[5]);
+        tv_hobby_1.setText(mTitleArray[0]);
+        tv_hobby_2.setText(mTitleArray[1]);
+        tv_hobby_3.setText(mTitleArray[2]);
+        tv_hobby_4.setText(mTitleArray[3]);
+        tv_hobby_5.setText(mTitleArray[4]);
+        tv_hobby_6.setText(mTitleArray[5]);
     }
 
     /**
@@ -173,6 +240,29 @@ public class UsedFragment extends BaseFragment implements HeaderScrollHelper.Scr
         pagerHeader.setAutoLoop(true, 3000);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_hobby_1:
+                UIUtils.snackBar(v, tv_hobby_1.getText().toString());
+                break;
+            case R.id.ll_hobby_2:
+                UIUtils.snackBar(v, tv_hobby_2.getText().toString());
+                break;
+            case R.id.ll_hobby_3:
+                UIUtils.snackBar(v, tv_hobby_3.getText().toString());
+                break;
+            case R.id.ll_hobby_4:
+                UIUtils.snackBar(v, tv_hobby_4.getText().toString());
+                break;
+            case R.id.ll_hobby_5:
+                UIUtils.snackBar(v, tv_hobby_5.getText().toString());
+                break;
+            case R.id.ll_hobby_6:
+                UIUtils.snackBar(v, tv_hobby_6.getText().toString());
+                break;
+        }
+    }
 
     private class HeaderAdapter extends PagerAdapter {
 
@@ -200,4 +290,5 @@ public class UsedFragment extends BaseFragment implements HeaderScrollHelper.Scr
             return view == object;
         }
     }
+
 }
